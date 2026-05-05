cce54a1ca38a80c45eb7480bbdffa58360bfb330
Nita Constantin <ctinnita88@gmail.com>
2026-05-05 22:05:06 +0300

    lets do it

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Http\Request as HttpRequest;
 
 class ChirpController extends Controller
 {
+    use AuthorizesRequests;
     public function index(){
         $chirps = Chirp::with('user')
         ->latest()
@@ -28,11 +30,39 @@ class ChirpController extends Controller
         ]);
 
         // Create the chirp (no user for now - we'll add auth later)
-        Chirp::create([
-            'message' =>$validated['message'],
-            'user_id' => null, // We'll add authentication in lesson 11
-        ]);
+        auth()->user()->chirps()->create($validated);
 
         return redirect('/')->with('success', 'Your chirp has been posted!');
+    }
+
+    public function edit(Chirp $chirp)
+    {
+        $this->authorize('update', $chirp);
+        return view('chirps.edit', compact('chirp'));
+    }
+
+    public function update(Request $request, Chirp $chirp)
+    {
+        $this->authorize('update', $chirp);
+         // Validate the request
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ],
+        [
+            'message.required' => 'Please write something to chirp!',
+            'message.max' => 'Chirps must be 255 characters or less'
+        ]);
+
+        // Update the chirp (no user for now - we'll add auth later)
+       $chirp->update($validated);
+
+        return redirect('/')->with('success', 'Your chirp has been updated!');
+    }
+
+    public function destroy( Chirp $chirp)
+    {
+        $this->authorize('update', $chirp);
+        $chirp->delete($chirp);
+        return redirect('/')->with('success', 'Your chirp has been deleted');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,44 @@
 <?php
 
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Register;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
 Route::get('/', [ChirpController::class, 'index']);
-Route::post('/chirps', [ChirpController::class, 'store']);
+
+
+Route::middleware('auth')->group(function(){
+    Route::post('/chirps', [ChirpController::class, 'store']);
+    Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+    Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+    Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+});
+
+
+
+// Shorter definition for the above routes
+// Route::resource('/chirps', [ChirpController::class])
+// ->only(['store', 'edit', 'update', 'destroy']);
+
+
+// Register routes
+Route::view('/register', 'auth.register')
+->middleware('guest')
+->name('register');
+Route::post('/register', Register::class)
+->middleware('guest');
+
+// Login
+Route::view('/login', 'auth.login')
+->middleware('guest')
+->name('login');
+
+Route::post('/login', Login::class)
+->middleware('guest');
+
+// Logout 
+Route::post('/logout', Logout::class)
+->middleware('auth')
+->name('logout');
+
